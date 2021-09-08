26e442b453af3228a0e931f96845258ac6d98728
Zuxelator <zuxel87@mail.ru>
2021-09-08 11:44:12 +0400

    1.1. Состояние нити. [#229175 #200192]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -9,7 +9,7 @@ public class ThreadState {
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED
-                && second.getState() != Thread.State.TERMINATED) {
+                || second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getName() + " : " + first.getState()
                       + " " + second.getName() + " : " + second.getState());
         }
