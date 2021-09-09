b16163ef13a8dda82ebeff0e4a93424501e078db
Zuxelator <zuxel87@mail.ru>
2021-09-09 23:03:53 +0400

    4. Скачивание файла с ограничением. [#144271]

---
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -0,0 +1,50 @@
+package ru.job4j.thread;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        /* Скачать файл*/
+        String file = url;
+        try (BufferedInputStream in = new BufferedInputStream(new URL(file).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("file.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long start = System.currentTimeMillis();
+            bytesRead = in.read(dataBuffer, 0, 1024);
+            long finish = System.currentTimeMillis();
+            long diff = finish - start;
+            int sleep = 0;
+            if (dataBuffer.length / diff < speed && diff != 0) {
+                sleep = dataBuffer.length / speed;
+            }
+            fileOutputStream.write(dataBuffer, 0, bytesRead);
+            Thread.sleep(sleep);
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                Thread.sleep(sleep);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
